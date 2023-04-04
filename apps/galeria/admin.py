6f77bc600ed4cfb525c36210aab8de88e6beabd1
Artur Santos <artursantos30.ars@gmail.com>
2023-04-04 16:00:56 -0400

    Refactoring style

--- apps/galeria/admin.py
+++ apps/galeria/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from galeria.models import Fotografia
+from apps.galeria.models import Fotografia
+
 
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "publicada")
@@ -8,5 +9,6 @@ class ListandoFotografias(admin.ModelAdmin):
     list_filter = ("categoria", "usuario",)
     list_editable = ("publicada",)
     list_per_page = 10
+
 
 admin.site.register(Fotografia, ListandoFotografias)
