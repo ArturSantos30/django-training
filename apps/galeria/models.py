175e6f4596498e5004c848cbfc52cb06d4f21981
Artur Santos <artursantos30.ars@gmail.com>
2023-04-12 17:37:21 -0400

    Adding page to insert a new post

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 
 class Fotografia(models.Model):
+    objects = models.Manager()
+
     OPCOES_CATEGORIA = [
         ("NEBULOSA", "Nebulosa"),
         ("ESTRELA", "Estrela"),
@@ -16,7 +18,7 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
-    publicada = models.BooleanField(default=False)
+    publicada = models.BooleanField(default=True)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     usuario = models.ForeignKey(
         to=User,
@@ -27,4 +29,4 @@ class Fotografia(models.Model):
     )
 
     def __str__(self):
-        return self.nome
+        return str(self.nome)
